ba79cd096648c522d046d21d4ab2ad1b3f5e23d5
motoba <motoba@waku-2.com>
2014-05-09 10:04:31 +0900

    first commit

---
+++ PodsTest/Global.h
@@ -0,0 +1,14 @@
+//
+//  Macro.h
+//  PodsTest
+//
+//  Created by 元場羊二郎 on 2014/05/08.
+//  Copyright (c) 2014年 com.test. All rights reserved.
+//
+
+#ifndef PodsTest_Macro_h
+#define PodsTest_Macro_h
+
+
+
+#endif
---
+++ PodsTest/PTViewController.h
@@ -0,0 +1,13 @@
+//
+//  PTViewController.h
+//  PodsTest
+//
+//  Created by 元場羊二郎 on 2014/05/08.
+//  Copyright (c) 2014年 com.test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PTViewController : UIViewController
+
+@end
