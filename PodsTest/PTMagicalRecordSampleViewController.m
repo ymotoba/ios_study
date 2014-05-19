8b6417fea73c46ff13faac3007e42d26b3b14b9d
motoba <motoba@waku-2.com>
2014-05-19 09:31:43 +0900

    delete insert周りの一部実装

--- PodsTest/PTMagicalRecordSampleViewController.m
+++ PodsTest/PTMagicalRecordSampleViewController.m
@@ -14,51 +14,52 @@
 
 @implementation PTMagicalRecordSampleViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
+	self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+	if (self) {
+		// Custom initialization
+	}
+	return self;
 }
 
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
-    [Person MR_truncateAll];
+- (void)viewDidLoad {
+	[super viewDidLoad];
+	// Do any additional setup after loading the view.
+//	[Person MR_truncateAll];
+	LOG(@"person count is %d.", [Person MR_countOfEntities]);
+	// select
+	Person *savedPerson = [Person MR_findFirstByAttribute:@"name" withValue:@"name test"];
+	if (!savedPerson) {
+        [self insertPerson];
+	}
+	else {
+        LOG(@"%@", savedPerson.address.zipCode);
+		// delete
+		BOOL deleteResult = [savedPerson MR_deleteEntity];
+		LOG(@"delete result is %d.", deleteResult);
+		// insert
+        [self insertPerson];
+	}
+	LOG(@"person count is %d.", [Person MR_countOfEntities]);
+}
 
+- (void)insertPerson {
     Person *person = [Person MR_createEntity];
     person.address = [Address MR_createEntity];
     person.name = @"name test";
-    person.address.zipCode = @"zipCode33333";
+    NSDate *datetime = [NSDate date];
+    NSDateFormatter *fmt = [[NSDateFormatter alloc] init];
+    fmt.dateFormat = @"yyyy/MM/dd HH:mm:ss";
+    person.address.zipCode = [fmt stringFromDate:datetime];
     person.address.state = @"state";
     person.address.city = @"city";
     person.address.other = @"other";
-    // select
-    Person *savedPerson = [Person MR_findFirstByAttribute:@"name" withValue:@"name test"];
-    LOG(@"%@", savedPerson.name);
-    LOG(@"%@", savedPerson.address.zipCode);
-    if (!savedPerson) {
-        // save
-        [[NSManagedObjectContext MR_defaultContext] MR_saveToPersistentStoreAndWait];
-    } else {
-        // update
-        NSDate *datetime = [NSDate date];
-        
-        NSDateFormatter *fmt = [[NSDateFormatter alloc] init];
-        fmt.dateFormat = @"yyyy/MM/dd HH:mm:ss";
-        savedPerson.address.zipCode = [fmt stringFromDate:datetime];
-        [[NSManagedObjectContext MR_defaultContext] MR_saveToPersistentStoreAndWait];
-    }
-    LOG(@"person count is %d.", [Person MR_countOfEntities]);
+    [[NSManagedObjectContext MR_defaultContext] MR_saveToPersistentStoreAndWait];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)didReceiveMemoryWarning {
+	[super didReceiveMemoryWarning];
+	// Dispose of any resources that can be recreated.
 }
 
 @end
