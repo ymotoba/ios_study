5a0699a72661025ebf339e57105289049be1f113
motoba <motoba@waku-2.com>
2014-05-21 17:50:45 +0900

    json2table用ViewControllerを追加

---
+++ PodsTest/PTJson2TableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PTJson2TableViewController.h
+//  PodsTest
+//
+//  Created by 元場羊二郎 on 2014/05/21.
+//  Copyright (c) 2014年 com.test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PTJson2TableViewController : UIViewController
+
+@end
---
+++ PodsTest/PTJson2TableViewController.m
@@ -0,0 +1,38 @@
+//
+//  PTJson2TableViewController.m
+//  PodsTest
+//
+//  Created by 元場羊二郎 on 2014/05/21.
+//  Copyright (c) 2014年 com.test. All rights reserved.
+//
+
+#import "PTJson2TableViewController.h"
+
+@interface PTJson2TableViewController ()
+
+@end
+
+@implementation PTJson2TableViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
